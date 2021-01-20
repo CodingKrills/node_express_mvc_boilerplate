3d0f4314b3d9e0b5871197181121c466d2fc7522
codingKrills <pulkitsinghmail@gmail.com>
2021-01-20 19:10:39 +0530

    updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = 3000
+const port = 8080
 
 // cors
 app.use(cors())
